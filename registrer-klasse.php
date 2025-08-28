ddd0a2549fc5bfe04504ff59faeb67d5a7832851
Nadsun <nadjasundal@icloud.com>
2025-08-28 13:09:12 +0200

    s

--- registrer-klasse.php
+++ registrer-klasse.php
@@ -1,15 +1,3 @@
-<?php
-$json = shell_exec("python3 /full/path/to/make_table.py");
-$data = json_decode($json, true);
-
-echo "<table border='1'>";
-echo "<tr><th>a</th><th>b</th><th>c</th></tr>";
-foreach ($data['a'] as $i => $a) {
-    echo "<tr>";
-    echo "<td>".$a."</td>";
-    echo "<td>".$data['b'][$i]."</td>";
-    echo "<td>".$data['c'][$i]."</td>";
-    echo "</tr>";
-}
-echo "</table>";
-?>
+<?php 
+	print ("<h3>Registrer klasse</h3>"); 
+?> 
