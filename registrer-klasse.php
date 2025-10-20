00adaab081aae3808df09b9db55ab1789258a8c5
Nadsun <nadjasundal@icloud.com>
2025-10-20 02:09:42 +0200

    sjsj

--- registrer-klasse.php
+++ registrer-klasse.php
@@ -1,3 +1,48 @@
+<?php  /* registrer-klasse */
+
+?> 
+
+<h3>Registrer Klasse </h3>
+
+<form method="post" action="" id="registrerKlasseSkjema" name="registrerKlasseSkjema">
+  Klassekode <input type="text" id="klassekode" name="klassekode" required /> <br/>
+  Klassenavn <input type="text" id="klassenavn" name="klassenavn" required /> <br/>
+  Studiumkode <input type="text" id="studiumkode" name="studiumkode" required /> <br/>
+  <input type="submit" value="Registrer klasse" id="registrerKlasseKnapp" name="registrerKlasseKnapp" /> 
+  <input type="reset" value="Nullstill" id="nullstill" name="nullstill" /> <br />
+</form>
+
 <?php 
-	print ("<h3>Registrer klasse</h3>"); 
+  if (isset($_POST ["registrerKlasseKnapp"]))
+    {
+      $klassekode=$_POST ["klassekode"];
+      $klassenavn=$_POST ["klassenavn"];
+      $studiumkode=$_POST ["studiumkode"];
+
+      if (!$klassekode || !$klassenavn || !$studiumkode)
+        {
+          print ("B&aring;de studiumkode og klassenavn m&aring; fylles ut");
+        }
+      else
+        {
+          include("db-tilkobling.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
+
+          $sqlSetning="SELECT * FROM klasse WHERE klassekode='$klassekode';";
+          $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
+          $antallRader=mysqli_num_rows($sqlResultat); 
+
+          if ($antallRader!=0)  /* klassekode er registrert fra før */
+            {
+              print ("Klassekode er registrert fra f&oslashr");
+            }
+          else
+            {
+              $sqlSetning="INSERT INTO klasse VALUES('$klassekode','$klassenavn','$studiumkode');";
+              mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; registrere data i databasen");
+                /* SQL-setning sendt til database-serveren */
+
+              print ("F&oslash;lgende studium er n&aring; registrert: $klassekode $klassenavn $studiumkode "); 
+            }
+        }
+    }
 ?> 
