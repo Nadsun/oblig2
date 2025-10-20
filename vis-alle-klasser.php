4fbdc3c48e6a8799aa3e661bf51b2d0a726b57d0
Nadsun <nadjasundal@icloud.com>
2025-10-20 02:26:13 +0200

    ff

--- vis-alle-klasser.php
+++ vis-alle-klasser.php
@@ -1,27 +1,28 @@
-<?php  /* vis-alle-studier */
+<?php  /* vis-alle-klasser */
 /*
-/*  Programmet skriver ut alle registrerte studiumer og emner
+/*  Programmet skriver ut alle registrerte klasser
 */
   include("db-tilkobling.php");  /* tilkobling til database-serveren utf�rt og valg av database foretatt */
 
-  $sqlSetning="SELECT * FROM studium;";
+  $sqlSetning="SELECT * FROM klasse;";
   
   $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
     /* SQL-setning sendt til database-serveren */
 	
   $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */
 
-  print ("<h3>Registrerte studium</h3>");
+  print ("<h3>Registrerte klasser</h3>");
   print ("<table border=1>");  
-  print ("<tr><th align=left>Studiumkode</th> <th align=left>Studiumnavn</th></tr>"); 
+  print ("<tr><th align=left>Klassekode</th> <th align=left>Klassenavn</th> <th align=left>Studiumkode</th></tr>"); 
 
   for ($r=1;$r<=$antallRader;$r++)
     {
       $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra spørringsresultatet */
-      $studiumkode=$rad["studiumkode"];        /* ELLER $postnr=$rad[0]; */
-      $studiumnavn=$rad["studiumnavn"];    /* ELLER $poststed=$rad[1]; */
+      $klassekode=$rad["klassekode"];        /* ELLER $postnr=$rad[0]; */
+      $klassenavn=$rad["klassenavn"]; 
+      $studiumkode=$rad["studiumkode"];    /* ELLER $poststed=$rad[1]; */
 
-      print ("<tr> <td> $studiumkode </td> <td> $studiumnavn </td> </tr>");
+    print ("<tr> <td> $klassekode </td> <td> $klassenavn </td> <td> $studiumkode </td> </tr>");
     }
   print ("</table>"); 
 ?>
